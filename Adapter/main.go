8177abd661163c895bf8dff8576d37f6dd3ed277
Annurdien Rasyid <ajwadax@gmail.com>
2021-08-01 15:00:24 +0700

    Adapter

---
+++ Adapter/main.go
@@ -0,0 +1,35 @@
+//main package has examples shown
+// in Hands-On Data Structures and algorithms with Go book
+package main
+// importing fmt package
+import (
+ "fmt"
+)
+//IProcess interface
+type IProcess interface {
+ process()
+}
+//Adapter struct
+type Adapter struct {
+ adaptee Adaptee
+}
+
+//Adapter class method process
+func (adapter Adapter) process() {
+ fmt.Println("Adapter process")
+ adapter.adaptee.convert()
+}
+//Adaptee Struct
+type Adaptee struct {
+ adapterType int
+}
+// Adaptee class method convert
+func (adaptee Adaptee) convert() {
+ fmt.Println("Adaptee convert method")
+}
+// main method
+func main() {
+var processor IProcess = Adapter{}
+processor.process()
+}
+
